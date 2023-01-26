8bf72e1d9221a5d1ae3d6284edaa2cc92f4c213c
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-01-26 15:19:05 -0800

    stripe fundaments

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,20 +1,41 @@
-class PaymentController {
-    constructor(subscriptionService){
-        this.subscriptionService = subscriptionService;
-    }
+const Stripe = require("stripe")
 
-    async getPaymentLink(req, res){
-        try {
-            const payment = await this.subscriptionService.createPayment();
-            return res.json(payment);
-        } catch (error) {
-            console.log(error)
 
-            return res
-                .status(500)
-                .json({ error: true, msg: "Failed to create payment"});
-        }
-    }
+const getPayment = async (id, amount, description) => {
+    const stripe = new Stripe(process.env.STRIPE_KEY)
+    const payment = stripe.paymentIntents.create({
+        amount:amount,
+        currency: "USD",
+        description: description,
+        payment_method: id,
+        confirm: true
+    })
+   
+    return payment
 }
 
-module.exports = PaymentController;
+
+
+
+
+
+// class PaymentController {
+//     constructor(subscriptionService){
+//         this.subscriptionService = subscriptionService;
+//     }
+
+//     async getPaymentLink(req, res){
+//         try {
+//             const payment = await this.subscriptionService.createPayment();
+//             return res.json(payment);
+//         } catch (error) {
+//             console.log(error)
+
+//             return res
+//                 .status(500)
+//                 .json({ error: true, msg: "Failed to create payment"});
+//         }
+//     }
+// }
+
+module.exports = getPayment;
