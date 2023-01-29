b9316a6f61b03af5f8966fd96bb5dbb9c67fd11a
Jhoalvi pereira <101520054+Jhoalvi25@users.noreply.github.com>
2023-01-29 12:17:07 -0500

    style mail

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -30,18 +30,18 @@ const sendEmail = async (email, subject, text) => {
       subject: `ShopLine`,
       text: `Email verification`,
       html: `
-        <div style='width: fit-content; height: 100vh; color:#1a0750; 
+        <div style='width: fit-content; height: 100vh; color:#5E17EB; 
                   text-align:center; '>
           <div style='background-color: white; padding:3em; width:100%; text-align: center;'>
             
             <div style='min-height: 6em; background:white; width:100%;'>
               <h1><strong>Welcome to ShopLine!</strong></h1>
             </div>
-            <img src='https://i.ibb.co/0J16yGS/Mi-proyecto.png' style='max-height: 150px' />
+            <img src='https://static.vecteezy.com/system/resources/previews/009/389/585/non_2x/orange-shopping-bag-icon-isolated-on-background-free-png.png' style='max-height: 150px' />
             <h2 style="opacity: .8">Click in the button bellow to complete your account verification</h2>
-            <a href=${text} style='padding: 1em; background-color: #1a0750; text-decoration:none; color: white;'
+            <a href=${text} style='padding: 1em; background-color: #5E17EB; text-decoration:none; color: white;'
                 onMouseOver="this.style.backgroundColor='#5519b6'; this.style.color='white'";
-                onMouseOut="this.style.backgroundColor='#1a0750'; this.style.color='white'">
+                onMouseOut="this.style.backgroundColor='#5E17EB'; this.style.color='white'">
               <strong>                           
                 Verify account
               </strong>
